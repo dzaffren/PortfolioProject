3d04276996b4f58a39fc50aadb0f37dbfddcb68f
dzaffren <105745417+dzaffren@users.noreply.github.com>
2022-06-13 17:31:02 +0800

    Add files via upload
    Updated version with 5 views

--- SQLCovidData.sql
+++ SQLCovidData.sql
@@ -81,6 +81,8 @@ SELECT *, (RollingPeopleVaccinated/Population)*100
 FROM PopvsVac
 
 --Creating view to store data for later visualisations
+
+--Percentage of Rolling Vaccination
 CREATE VIEW PercententageRollingVaccinantion AS
 SELECT dea.continent, dea.location, dea.date, dea.population, vac.new_vaccinations, SUM(cast(vac.new_vaccinations as int)) OVER (PARTITION BY dea.location ORDER BY dea.location, dea.date)
 as RollingPeopleVaccinated
@@ -91,5 +93,32 @@ JOIN PortfolioProjects..CovidVaccinations$ vac
 WHERE dea.continent IS NOT NULL AND dea.population IS NOT NULL
 --ORDER BY 2,3
 
-SELECT *
-FROM PercententageRollingVaccinantion
+--Global Numbers
+CREATE VIEW GlobalNumbers AS
+SELECT SUM(new_cases) as TotalCases, SUM(cast(new_deaths AS int)) as TotalDeaths, (SUM(cast(new_deaths AS int))/SUM(new_cases))*100 AS DeathPercentage
+FROM PortfolioProjects..CovidDeaths$
+WHERE continent IS NOT NULL
+--GROUP BY date
+--ORDER BY 1, 2 
+
+--Total Case v Population
+CREATE VIEW TotalCasePop AS
+SELECT location, date, total_cases, population, (total_deaths/population)*100 AS DeathPercentage
+FROM PortfolioProjects..CovidDeaths$
+WHERE continent IS NOT NULL
+--ORDER BY 1, 2
+
+--Total Case v Total Death
+CREATE VIEW TotalCaseDeath AS
+SELECT location, date, total_cases, total_deaths, (total_deaths/total_cases)*100 AS DeathPercentage
+FROM PortfolioProjects..CovidDeaths$
+WHERE continent IS NOT NULL
+--ORDER BY 1, 2
+
+--Highest Infection per Population
+CREATE VIEW HighInfect AS
+SELECT location, population, MAX(total_cases) AS HighestInfection, MAX((total_cases/population))*100 AS InfectedPercentage
+FROM PortfolioProjects..CovidDeaths$
+WHERE continent IS NOT NULL
+GROUP BY location, population
+--ORDER BY InfectedPercentage DESC
